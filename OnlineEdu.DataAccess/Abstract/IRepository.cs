cdac9736317f1b8e13b8307f2488ddbe2660d443
metndurn <mtndrn123@gmail.com>
2025-07-29 22:12:02 +0300

    Asp.Net Core 8.0 Web API OnlineEdu Projesi - IRepository ve GenericRepository

---
+++ OnlineEdu.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.DataAccess.Abstract
+{
+	public interface IRepository<T> where T : class
+	{
+		List <T> GetList();
+		T GetByFilter(Expression<Func<T, bool>> predicate);//sarta gore tek bır deger getırme ısıne yarar
+		T GetById(int id);
+		void Create(T entity);
+		void Update(T entity);
+		void Delete(int id);
+		int Count();
+		int FilteredCount(Expression<Func<T, bool>> predicate);//sarta gore butun sayıların degerini getırme ısıne yarar
+		List<T> GetFilteredList(Expression<Func<T,bool>>predicate);//sarta gore butun degeri getırme ısıne yarar
+	}
+}
